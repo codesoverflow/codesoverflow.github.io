601a774c81b20549ef8825c98461f439f8b83b67
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-03-10 14:56:22 +0530

    Fix UI

--- assets/md.transition-legacy-CCBaLc49.js
+++ assets/md.transition-legacy-CCBaLc49.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wmxueRR1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-00dHhatm.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
