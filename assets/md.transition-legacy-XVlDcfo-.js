c1cec33de1be96522ed5ce6321f71ae0c11b4503
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-04-28 14:28:12 +0530

    Update syncing logic

--- assets/md.transition-legacy-XVlDcfo-.js
+++ assets/md.transition-legacy-XVlDcfo-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-00dHhatm.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-T_VoCt-v.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
