801e288ae45ff184267756299fe4b666163376d9
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-05-30 19:36:00 +0530

    update ui

--- assets/ios.transition-legacy-BW8A7jnh.js
+++ assets/ios.transition-legacy-BW8A7jnh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dqpiq6lQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C8hwAw8y.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
