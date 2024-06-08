3c8031bbfaad83b657751a92d0df60b57ebe14cc
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-06-08 11:37:54 +0530

    Update sync

--- assets/ios.transition-legacy-mvXi9Lcs.js
+++ assets/ios.transition-legacy-mvXi9Lcs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CK9RUG80.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-emSVSi9F.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
